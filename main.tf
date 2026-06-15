4f0b2bf2293d54af2ba4eeb7189c0f638e589c91
Kumar Shubham <kmrshubham@google.com>
2026-06-15 10:52:20 +0530

    Diagnostic release 1.0.2

--- main.tf
+++ main.tf
@@ -1,18 +1,4 @@
-resource "google_storage_bucket" "bucket" {
-  project                     = var.project_id
-  name                        = var.name
-  location                    = var.location
-  storage_class               = var.storage_class
-  force_destroy               = var.force_destroy
-  labels                      = var.labels
-  uniform_bucket_level_access = true
-  public_access_prevention    = "enforced"
-
-  versioning {
-    enabled = var.versioning_enabled
-  }
-
-  encryption {
-    default_kms_key_name = var.kms_key_name
-  }
+# Minimal diagnostic main.tf
+locals {
+  diagnostic_test = "hello-world"
 }
